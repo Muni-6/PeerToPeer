5a66c70c6b02edf688b894249df611e282de0742
Muni-6 <90185574+Muni-6@users.noreply.github.com>
2024-09-06 14:16:37 -0400

    Delete Downloads/Chat_Application directory

--- Downloads/Chat_Application/auth_backend/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import verifyToken from "../middleware/verifyToken.js";
-import getUsers from "../controller/user.controller.js";
-
-const router = express.Router();
-router.get('/', verifyToken, getUsers);
-
-export default router;
